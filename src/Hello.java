0084866ce20d0aa4c81a14df3ce1597debdd11f4
Elyssa Guajardo <101149603+nyoomitsellie@users.noreply.github.com>
2025-01-30 21:02:25 +0000

    finished

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,8 @@
-// Hello World
+public class Hello {
+
+    // Let's create our work area
+    public static void main(String[] args) {
+        // print out hello world
+        System.out.println("Hello World!");
+    }
+}
